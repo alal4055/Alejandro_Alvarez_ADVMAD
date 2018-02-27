c796c87016e77e62a79ac7c19e30d4dd7b790957
alal4055 <alal4055@colorado.edu>
2018-02-27 11:56:07 -0700

    Add files via upload

---
+++ Lab 3/Lab 3/Lab 3/DetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  DetailViewController.swift
+//  Lab 3
+//
+//  Created by Alejandro Alvarez on 2/26/18.
+//  Copyright © 2018 Alejandro Alvarez. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var websiteView: WKWebView!
+    @IBOutlet weak var webSpinner: UIActivityIndicatorView!
+    
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+                loadWebsite(detail.description)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        websiteView.navigationDelegate = self
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func loadWebsite(_ urlString: String){
+        let myurl = URL(string: urlString)
+        //create a URLRequest object
+        let request = URLRequest(url: myurl!)
+        //load the URLRequest object in our web view
+        websiteView.load(request)
+    }
+    
+    //WKNavigationDelegate method that is called when a web page begins to load
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        webSpinner.startAnimating()
+    }
+    
+    //WKNavigationDelegate method that is called when a web page loads successfully
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webSpinner.stopAnimating()
+    }
+}
+
