bc2ea4c4a0db6cb6426879cfaf3e3287f670a217
alal4055 <alal4055@colorado.edu>
2018-03-13 12:11:24 -0600

    Add files via upload

---
+++ Project 1 -Milestone3/Project 1/character.swift
@@ -0,0 +1,14 @@
+//
+//  character.swift
+//  Project 1
+//
+//  Created by Alejandro Alvarez on 3/12/18.
+//  Copyright © 2018 Alejandro Alvarez. All rights reserved.
+//
+
+import Foundation
+
+class Characters {
+    var characterData = [[String : String]]()
+    var fact = [String]()
+}
---
+++ Project 1 -Milestone3/Project 1/votes.swift
@@ -0,0 +1,14 @@
+//
+//  votes.swift
+//  Project 1
+//
+//  Created by Alejandro Alvarez on 3/11/18.
+//  Copyright © 2018 Alejandro Alvarez. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Votes: Codable {
+    var nameCharacter: String
+}
